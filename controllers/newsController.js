05043711977d71a62bd4f5c6a026570c5537360d
System Administrator <alexgoiadev@gmail.com>
2019-12-24 18:26:11 +0100

    Basic new controllers

---
+++ controllers/newsController.js
@@ -0,0 +1,61 @@
+const
+    express = require('express'),
+    router = express.Router();
+
+router.route('/news/:id')
+    .get(async (req, res) => {
+        try {
+
+        } catch (error) {
+            res.status(400).send({
+                status: 400,
+                error
+            })
+        }
+    })
+    .put(async (req, res) => {
+        try {
+
+        } catch (error) {
+            res.status(400).send({
+                status: 400,
+                error
+            })
+        }
+    })
+    .delete(async (req, res) => {
+        try {
+
+        } catch (error) {
+            res.status(400).send({
+                status: 400,
+                error
+            })
+        }
+    });
+
+router.route('/news')
+    .get(async (req, res) => {
+        const page = req.query.page || 0;
+        const limit = req.query.limit || 5;
+        try {
+
+        } catch (error) {
+            res.status(400).send({
+                status: 400,
+                error
+            })
+        }
+    })
+    .post(async (req, res) => {
+        try {
+
+        } catch (error) {
+            res.status(400).send({
+                status: 400,
+                error
+            })
+        }
+    });
+
+module.exports = router;
---
+++ models/newsModel.js
@@ -0,0 +1,32 @@
+const
+    mongoose = require('mongoose'),
+    mongoosePaginate = require('mongoose-paginate-v2');
+
+const News = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    archiveDatge: {
+        type: Date
+    }
+});
+
+News.plugin(mongoosePaginate);
+module.exports = mongoose.model('News', News);
