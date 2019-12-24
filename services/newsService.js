bff7c52db6d3fd8be82ef903246679afbc6cbef7
System Administrator <alexgoiadev@gmail.com>
2019-12-24 18:33:07 +0100

    Created newsService

--- services/newsService.js
+++ services/newsService.js
@@ -1,23 +1,24 @@
 
+const News = require('./../models/newsModel');
 
 const addNews = async (news) => {
-
+    return await new News(news).save();
 }
 
-const getNews = async (limit, page) => {
-
+const getNews = async (limit, offset) => {
+    return await News.paginate({}, { limit, offset });
 }
 
 const getNewsById = async (id) => {
-    
+    return await News.findById({ _id: id });
 }
 
 const updateNewsById = async (id, changes) => {
-
+    return await News.findByIdAndUpdate({ _id, id }, changes, { new: true });
 }
 
 const deleteNewsById = async (id) => {
-
+    return await News.findByIdAndRemove({ _id: id });
 }
 
 
