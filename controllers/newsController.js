369feab6e3dfb100a3310a849d3cdd3f1712f3bf
System Administrator <alexgoiadev@gmail.com>
2019-12-24 18:50:34 +0100

    Preparada conexion y server.js

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -1,11 +1,17 @@
 const
     express = require('express'),
-    router = express.Router();
+    router = express.Router(),
+    newsService = require('./../services/newsService');
 
 router.route('/news/:id')
     .get(async (req, res) => {
         try {
-
+            const data = await newsService.getNewsById(req.params.id);
+            res.status(200).send({
+                status: 200,
+                message: 'Success',
+                data
+            });
         } catch (error) {
             res.status(400).send({
                 status: 400,
@@ -15,7 +21,12 @@ router.route('/news/:id')
     })
     .put(async (req, res) => {
         try {
-
+            const data = await newsService.updateNewsById(req.params.id, req.body);
+            res.status(200).send({
+                status: 200,
+                message: 'Success',
+                data
+            });
         } catch (error) {
             res.status(400).send({
                 status: 400,
---
+++ server.js
@@ -0,0 +1,39 @@
+const
+    express = require('express'),
+    app = express(),
+    PORT = 3333,
+    cors = require('cors'),
+    compression = require('compression'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    config = require('./config.json'),
+    /* Controlloers */
+    newsController = require('./controllers/newsController');
+
+app.use(cors({ origin: '*' }));
+app.use(compression());
+app.use(bodyParser.json({ limit: '10mb', extended: true }));
+
+/* ENDPOINTS */
+app.use(newsController);
+
+/* CONEXIÓN CON LA BASE DE DATOS */
+mongoose.connect(`mongodb+srv://${config.username}:${config.password}@${config.host}/${config.database}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(
+    () => {
+        console.log(`¡Conectado a la base de datos! ${config.host}`);
+        console.log(`** Conexión para hacer pruebas y tests **`);
+    }
+).catch((err) => {
+    console.error(`ERROR MONGODB: ${err}`);
+});
+
+
+app.listen(PORT, () => {
+    console.log(`API RUNNING ON PORT ${PORT}`);
+});
+
+
