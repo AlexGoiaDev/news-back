08bcdfaec379f74b94c7917acdcdc64247d48dec
System Administrator <alexgoiadev@gmail.com>
2019-12-24 18:29:51 +0100

    Created basic services structure

---
+++ services/newsService.js
@@ -0,0 +1,30 @@
+
+
+const addNews = async (news) => {
+
+}
+
+const getNews = async (limit, page) => {
+
+}
+
+const getNewsById = async (id) => {
+    
+}
+
+const updateNewsById = async (id, changes) => {
+
+}
+
+const deleteNewsById = async (id) => {
+
+}
+
+
+module.exports = {
+    addNews,
+    getNews,
+    getNewsById,
+    updateNewsById,
+    deleteNewsById
+}
